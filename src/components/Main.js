463043fe277a83d1c44a75696c514a8d9e9bd171
Svmpankaj <svmpankaj@gmail.com>
2024-02-11 23:57:02 +0530

    adding more data at amin page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import useState from 'react';
 
 const Main = () => {
+    const [first, setfirst] = useState(second)
     return (
         <div>
-            This is main page
+            {/* This is main page */}
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat eligendi autem dolores blanditiis aperiam animi rerum? Autem cumque optio accusamus voluptates fugiat quidem nostrum nisi? Autem, facilis porro? Quis illo, facere suscipit veniam possimus, dolore temporibus deserunt ea obcaecati eum voluptatibus voluptatum consequuntur atque fugit alias consectetur culpa assumenda quibusdam soluta commodi in reprehenderit! Exercitationem atque facere veritatis aliquid sequi adipisci mollitia aliquam corporis vel necessitatibus autem natus, minus pariatur enim porro temporibus quo et magni totam earum delectus unde. Quam neque numquam est, ab tempore ut, aperiam officia quas magni labore eligendi culpa amet molestias quia repellendus nostrum fugiat.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, labore cumque. Dolor sit eius nihil libero laborum. Ratione eum, magnam accusantium exercitationem rerum quasi. Veniam aperiam possimus eius ullam ipsam.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum ut amet consequuntur provident iusto. Sed, distinctio. Veritatis id dicta quod deserunt distinctio provident aut praesentium reprehenderit tempora expedita consequatur delectus neque laudantium illo autem, a, repellendus nihil? Ullam libero eveniet omnis doloribus quaerat, quisquam repellat dignissimos, iste rerum eaque unde beatae, cupiditate voluptas! Voluptas quia, dolorum voluptatum, excepturi sunt perspiciatis nobis corrupti, officia tempora dolore aperiam accusamus labore quidem temporibus.
+
+            </p>
 
         </div>
     )
