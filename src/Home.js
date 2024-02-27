99d68432b89cb5424d7fb0de237b730f5c4596d1
Svmpankaj <svmpankaj@gmail.com>
2024-02-27 21:00:48 +0530

    adding click event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log('hello, ninjas', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('hello' + name, e.target);
+    }
+
     return (
         <div className="home">
             <h1>Homepage</h1>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('ram', e)}>Click me again</button>
         </div>
     )
 }
