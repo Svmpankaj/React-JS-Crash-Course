b08bd194ec78b7565cebd7e1ee2b80b54bcf3d84
Svmpankaj <svmpankaj@gmail.com>
2024-03-13 17:20:26 +0530

    adding handle click

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,19 @@
 
 const Home = () => {
 
+    const handleClick = () => {
+        console.log('hello, ninjas');
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('click me again', name);
+    }
 
     return (
         <div className="home">
             <h2>homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => handleClickAgain('ram')}>Click me again</button>
         </div>
     )
 }
